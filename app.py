3cef0d74e4a26c34f621b716d27684f2e8fe1898
enkhjil <enkhjil@data-artist.com>
2020-03-31 15:54:57 +0900

    demo

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/webhooks', methods=['GET'])
+@app.route('/webhook', methods=['GET'])
 def verify():
     VERIFICATION_TOKEN = os.environ.get('VERIFICATION_TOKEN')
     token_sent = request.args.get('hub.verify_token')
     if token_sent == VERIFICATION_TOKEN:
         print('Verified!')
         return request.args.get('hug.challenge')
-    else:
-        return 'Invalid verification token!'
+    return 'Invalid verification token!'
+
+
+if __name__ == '__main__':
+    app.run()
